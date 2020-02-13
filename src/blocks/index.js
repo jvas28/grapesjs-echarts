62205d4481615c99b3566e54f6f29d69c5ef8aa8
Julio Vasconez <j.vasconez@airlst.com>
2020-02-13 17:08:33 +0100

    Pie Chart basic support

--- src/blocks/index.js
+++ src/blocks/index.js
@@ -1,6 +1,8 @@
 import CustomChart from "./custom-chart";
 import BarsChart from "./bars-chart";
+import PieChart from "./pie-chart";
 export default {
 	"custom-chart": CustomChart,
-	"bars-chart": BarsChart
+	"bars-chart": BarsChart,
+	"pie-chart": PieChart
 };
--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,8 @@
 import EchartsCustom from "./echarts-custom";
 import Bars from "./echarts-bars";
+import Pie from "./echarts-pie";
 export default {
 	"echarts-custom": EchartsCustom,
 	"echarts-bars": Bars,
+	"echarts-pie": Pie,
 };
